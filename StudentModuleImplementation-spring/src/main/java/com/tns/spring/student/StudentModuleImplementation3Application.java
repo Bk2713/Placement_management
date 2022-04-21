dd8b6457313c4a9655dd585c98aca6305ea8d19f
Administrator <Administrator@192.168.1.6>
2022-04-20 18:23:54 -0700

    Placement management -Spring

---
+++ StudentModuleImplementation-spring/src/main/java/com/tns/spring/student/StudentModuleImplementation3Application.java
@@ -0,0 +1,13 @@
+package com.tns.spring.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentModuleImplementation3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentModuleImplementation3Application.class, args);
+	}
+
+}
---
+++ StudentModuleImplementation-spring/src/test/java/com/tns/spring/student/StudentModuleImplementation3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.spring.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentModuleImplementation3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
